15f271eac3eee8b980a91976ae3629e26cdc7b10
Hobson Nascimento <hobsonbmn@gmail.com>
2021-11-14 13:14:19 -0300

    Atualização de projeto

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <title>Gerencia Sorvete Mania</title>
+    <title>Gerência Sorvete Mania</title>
     <link rel="stylesheet" href="_css/estilo.css">
 </head>
 
