dac8c9a11923d95cff197df7188b896c8bc43903
Hobson Nascimento <hobsonbmn@gmail.com>
2021-11-16 13:45:07 -0300

    atualizações de commit07

--- index.php
+++ index.php
@@ -29,18 +29,32 @@
         </nav>
         <section>
 
-            <h1>CADASTRO DE DESPESAS</h1>
+            <h1>Contagem Diária</h1>
             <hr><br><br>
             <form method=" POST" action="processa.php">
                 <input type="submit" value="Salvar" class="btn">
                 <input type="reset" value="Limpar" class="btn">
                 <br><br>
 
-                Item<br>
+                Cone Casquinha<br>
                 <input type="text" name="item" class="campo" maxlength="40" required autofocus><br>
-                Qantidade<br>
+                Cone Cascão<br>
                 <input type="numero" name="quantidade" class="campo" maxlength="40" required><br>
-                Valor<br>
+                Copo 100 ml<br>
+                <input type="numero" name="quantidade" class="campo" maxlength="40" required><br>
+                Copo de Sundae<br>
+                <input type="numero" name="valor" class="campo" maxlength="40" required><br>
+                Copo de MilkShake 300 ml<br>
+                <input type="numero" name="valor" class="campo" maxlength="40" required><br>
+                Copo de MilkShake 400 ml<br>
+                <input type="numero" name="valor" class="campo" maxlength="40" required><br>
+                Copo de MilkShake 500 ml<br>
+                <input type="numero" name="valor" class="campo" maxlength="40" required><br>
+                Cobertura de Chocolate<br>
+                <input type="numero" name="valor" class="campo" maxlength="40" required><br>
+                Cobertura de Morango<br>
+                <input type="numero" name="valor" class="campo" maxlength="40" required><br>
+                Cobertura de Caramelo<br>
                 <input type="numero" name="valor" class="campo" maxlength="40" required><br>
             </form>
         </section>
